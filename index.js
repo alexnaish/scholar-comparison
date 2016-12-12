b14ba77746413637ba457ad78d3f43012b91e9f7
Noah <nowarkdev@gmail.com>
2016-12-12 15:53:13 +0000

    @alexnaish update resemble options to be more inline with scholar server

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const async = require('async');
 
 const resemble = require('./resemble');
 
+resemble.setErrorRgbColour(255, 0, 0);
+resemble.setErrorOpacity(0.4);
+resemble.setLargeImageThreshold(2400);
+
 module.exports = function(image1, image2, callback) {
     resemble(image1)
         .compareTo(image2)
